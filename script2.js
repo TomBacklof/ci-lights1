6876ca9a2a9d7dc1f09df9732dfcd7ea58ece2a3
TomBacklof <54667973+TomBacklof@users.noreply.github.com>
2019-10-08 16:00:45 +0200

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,141 @@
+fetch('https://ci-lights.azurewebsites.net', {
+
+	method: 'post',
+
+	body: JSON.stringify({
+
+        {
+
+            "set": [
+            {
+                "id": 1,
+                "color": [200,200,0]
+            },
+            {
+                "id": 2,
+                "color": [200,0,20]
+            }
+            {
+                "id": 3,
+                "color": [200,0,20]
+            }
+            {
+                "id": 4,
+                "color": [200,0,20]
+            }
+            {
+                "id": 5,
+                "color": [200,0,20]
+            }
+            {
+                "id": 6,
+                "color": [200,0,20]
+            }],
+            "session": "main"
+        }
+            
+        {
+
+            "set": [
+            {
+                "id": 7,
+                "color": [200,200,0]
+            },
+            {
+                "id": 8,
+                "color": [200,0,20]
+            }
+            {
+                "id": 9,
+                "color": [200,0,20]
+            }
+            {
+                "id": 10,
+                "color": [200,0,20]
+            }
+            {
+                "id": 11,
+                "color": [200,0,20]
+            }
+            {
+                "id": 12,
+                "color": [200,0,20]
+            }],
+            "session": "main"
+        }
+            
+        {
+
+            "set": [
+            {
+                "id": 13,
+                "color": [200,200,0]
+            },
+            {
+                "id": 14,
+                "color": [200,0,20]
+            }
+            {
+                "id": 15,
+                "color": [200,0,20]
+            }
+            {
+                "id": 16,
+                "color": [200,0,20]
+            }
+            {
+                "id": 17,
+                "color": [200,0,20]
+            }
+            {
+                "id": 18,
+                "color": [200,0,20]
+            }],
+            "session": "main"
+        }
+
+        {
+
+            "set": [
+            {
+                "id": 19,
+                "color": [200,200,0]
+            },
+            {
+                "id": 20,
+                "color": [200,0,20]
+            }
+            {
+                "id": 21,
+                "color": [200,0,20]
+            }
+            {
+                "id": 22,
+                "color": [200,0,20]
+            }
+            {
+                "id": 23,
+                "color": [200,0,20]
+            }
+            {
+                "id": 24,
+                "color": [200,0,20]
+            }],
+            "session": "main"
+        }
+            
+            
+           
+	// headers: {
+
+	//	contentType: 'application/json'
+
+	// }
+
+
+    //blackout_schema = {
+   // 'required': ['session'],
+    //'properties': {
+      //  'session': {'type': 'string'}
+   // }
+// }
