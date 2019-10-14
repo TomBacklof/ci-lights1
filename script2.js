8229ebe044d53ab96e7e2de7e42884c2ac90f7d9
TomBacklof <54667973+TomBacklof@users.noreply.github.com>
2019-10-14 12:41:35 +0200

    Update script2.js

--- script2.js
+++ script2.js
@@ -1,141 +1,25 @@
-fetch('https://ci-lights.azurewebsites.net', {
-
-	method: 'post',
-
-	body: JSON.stringify({
-
-        {
-
-            "set": [
-            {
-                "id": 1,
-                "color": [200,200,0]
+function postColor(colors, id) {
+    fetch('https://ci-lights.azurewebsites.net/setcolor', {
+            method: 'post',
+            body: JSON.stringify({
+                id: id,
+                color: colors,
+                session: 'main',
+            }),
+            headers: {
+                'Content-Type': 'application/json',
             },
-            {
-                "id": 2,
-                "color": [200,0,20]
-            }
-            {
-                "id": 3,
-                "color": [200,0,20]
-            }
-            {
-                "id": 4,
-                "color": [200,0,20]
-            }
-            {
-                "id": 5,
-                "color": [200,0,20]
-            }
-            {
-                "id": 6,
-                "color": [200,0,20]
-            }],
-            "session": "main"
-        }
-            
-        {
-
-            "set": [
-            {
-                "id": 7,
-                "color": [200,200,0]
-            },
-            {
-                "id": 8,
-                "color": [200,0,20]
-            }
-            {
-                "id": 9,
-                "color": [200,0,20]
-            }
-            {
-                "id": 10,
-                "color": [200,0,20]
-            }
-            {
-                "id": 11,
-                "color": [200,0,20]
-            }
-            {
-                "id": 12,
-                "color": [200,0,20]
-            }],
-            "session": "main"
-        }
-            
-        {
-
-            "set": [
-            {
-                "id": 13,
-                "color": [200,200,0]
-            },
-            {
-                "id": 14,
-                "color": [200,0,20]
-            }
-            {
-                "id": 15,
-                "color": [200,0,20]
-            }
-            {
-                "id": 16,
-                "color": [200,0,20]
-            }
-            {
-                "id": 17,
-                "color": [200,0,20]
-            }
-            {
-                "id": 18,
-                "color": [200,0,20]
-            }],
-            "session": "main"
-        }
-
-        {
-
-            "set": [
-            {
-                "id": 19,
-                "color": [200,200,0]
-            },
-            {
-                "id": 20,
-                "color": [200,0,20]
-            }
-            {
-                "id": 21,
-                "color": [200,0,20]
-            }
-            {
-                "id": 22,
-                "color": [200,0,20]
-            }
-            {
-                "id": 23,
-                "color": [200,0,20]
-            }
-            {
-                "id": 24,
-                "color": [200,0,20]
-            }],
-            "session": "main"
-        }
-            
-            
-           
-	// headers: {
-
-	//	contentType: 'application/json'
-
-	// }
+        }).then(res => console.log(res));
+}
 
 
-    //blackout_schema = {
-   // 'required': ['session'],
-    //'properties': {
-      //  'session': {'type': 'string'}
-   // }
-// }
+if (kickCount > 0) {
+        KickDrum.triggerAttack('C2');
+        kickCount = 0;
+        canAddKickDrum = false;
+        setTimeout(function() {
+            canAddKickDrum = true;
+        }, 500);
+        
+        postColor([255, 0, 0])
+    }
